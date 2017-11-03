8c4e94d17fdcf468fce5d3d7bcc8da05f4bb1370
Oleg Sulzhenko <oleg.sulzhenko@perfectial.com>
2017-11-03 13:10:15 +0200

    origin 2

--- api/serverCall.php
+++ api/serverCall.php
@@ -52,7 +52,7 @@ function requestPaymentData($order, $server, $authentication)
 
         /** All server specific settings can be found in config/server.php */
 
-        'origin' => 'https://checkoutshopper-test.adyen.com',
+        'origin' => 'https://adyen-checkout-aleg.herokuapp.com',
         'shopperIP' => $server['shopperIP'],
         'returnUrl' => $server['returnURL'],
 
