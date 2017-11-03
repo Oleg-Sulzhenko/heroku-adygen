027a0ecbf53ae1bcada030779b96f03ca983ba82
Oleg Sulzhenko <oleg.sulzhenko@perfectial.com>
2017-11-03 13:14:50 +0200

    asda

--- config/server.php
+++ config/server.php
@@ -40,7 +40,7 @@ $returnURL = url();
 $shopperIP = $_SERVER['REMOTE_ADDR'];
 
 return [
-    'origin' => url(),
+    'origin' => 'https://checkoutshopper-test.adyen.com',
     'baseURL' => $checkoutBaseURL,
     'setupURL' => $checkoutSetupURL,
     'verifyURL' => $checkoutVerifyURL,
